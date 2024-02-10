04aaf41f2425a58f7a63565eaaadca39fd797232
TugbaBingol <tugbaabiingoll@gmail.com>
2024-02-10 15:50:12 +0300

    Kurs Ödevi

---
+++ Workaround/Program.cs
@@ -0,0 +1,29 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {//sdfs
+        static void Main(string[] args)
+        {
+
+            Person person1 = new Person();
+            person1.FirstName = "Tuğba";
+            person1.LastName = "Bingöl";
+            person1.DateOfBirthYear = 2000;
+            person1.NationalIdentity = 123;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+
+            Console.ReadLine();
+
+        }
+
+        
+    }
+
+}
